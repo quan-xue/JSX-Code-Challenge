7703618fa8788af2f9eb8e3c49d200b30074d000
Quan Xue <33763102+quan-xue@users.noreply.github.com>
2020-10-03 08:47:16 +0800

    Complete challenge Create list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
 //It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+ReactDOM.render(
+  //It should display an unordered list (bullet points).
+  //It should contain 3 list elements.
+  <ul>
+    My favourite foods.
+    <li>Chicken</li>
+    <li>Beef</li>
+    <li>Pork</li>
+  </ul>,
+  document.getElementById("root")
+);
